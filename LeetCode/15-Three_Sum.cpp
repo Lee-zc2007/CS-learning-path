7d5a8dcfffc7dd4b6299c54f5fa602a9c75bac86
Lee_zc2007 <Lee_zc2007@163.com>
2026-08-01 19:22:14 +0800

    finish three sum

--- LeetCode/15-Three_Sum.cpp
+++ LeetCode/15-Three_Sum.cpp
@@ -6,30 +6,32 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        int last_k, k, i, last_i, last_j, j;
         sort(nums.begin(), nums.end());
-        last_k = *nums.begin();
-        k = 0;
-        while (k < nums.size()-1) {
-            i = k + 1;
+        int k=0,i,j;
+        while (nums[k] <= 0 && k < nums.size()-2){
+            i = k+1;
             j = nums.size()-1;
-            while (i != j){
-                int sum = nums[k] + nums[i] + nums[j];
-                last_i = nums[i];
-                last_j = nums[j];
-                if (sum > 0) {
-                    while (nums[j] == last_j) j--;
+            while (i < j){
+                if (nums[k] + nums[i] + nums[j] < 0){
+                    int last_i = i;
+                    do {i++;}
+                    while (nums[i] == nums[last_i] && i<j);
                 }
-                else if (sum < 0){
-                    while (nums[i] == last_i) i++;
+                else if (nums[k] + nums[i] + nums[j] > 0){
+                    int last_j = j;
+                    do {j--;}
+                    while (nums[j] == nums[last_j] && i<j);
                 }
-                else {
-                    ans.push_back(vector<int> {nums[k], nums[i], nums[j]});
-                    while (nums[k] == last_k) k++;
-                    last_k = nums[k];
-                    break;
+                else{
+                    ans.push_back(vector<int>{nums[k], nums[i], nums[j]});
+                    int last_i = i;
+                    do {i++;}
+                    while (nums[i] == nums[last_i] && i<j);
                 }
             }
+            int last_k = k;
+            do {k++;}
+            while (nums[k] == nums[last_k]);
         }
         return ans;
     }
